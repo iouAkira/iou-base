4c692cf85da679709054ae53940b0bdf8e45a052
Akira <e.akimoto.akira@gmail.com>
2021-11-09 14:19:15 +0800

    init

--- repo_sync/repo_sync.go
+++ repo_sync/repo_sync.go
@@ -15,27 +15,24 @@ import (
 )
 
 func main() {
-	// ENV VER=0.1 \
-    // MNT_DIR=/data \
-    // CRON_FILE_PATH=/iouCron \
-	mntDir="/data"
-	repoBaseDir="/iouRepos"
-	
+	mntDir := "/data"
+	repoBaseDir := "/iouRepos"
+
 	if os.Getenv("MNT_DIR") != "" {
 		mntDir = os.Getenv("MNT_DIR")
 	}
 	if os.Getenv("REPOS_DIR") != "" {
 		repoBaseDir = os.Getenv("REPOS_DIR")
 	}
-	repoConfigJson=fmt.Sprintf("%v/repos.json",mntDir)
+	repoConfigJson := fmt.Sprintf("%v/repos.json", mntDir)
 	if os.Getenv("REPOS_CONFIG") != "" {
 		repoConfigJson = os.Getenv("REPOS_CONFIG")
 	}
-	
-	readRepoConfig(repoConfigJson,repoBaseDir)
+
+	readRepoConfig(repoConfigJson, repoBaseDir)
 }
 
-func readRepoConfig(repoConfigJson string,repoBaseDir string) {
+func readRepoConfig(repoConfigJson string, repoBaseDir string) {
 	if rs.Exists(repoConfigJson) {
 		log.Printf("检测到仓库配置文件 %v，开始同步仓库操作。", repoConfigJson)
 		var repoConfig rs.ReposConfig
@@ -46,7 +43,7 @@ func readRepoConfig(repoConfigJson string,repoBaseDir string) {
 				if repo.RepoPrivate {
 					if repo.GitAccount != "" && repo.GitToken != "" {
 						log.Printf("↓↓↓↓↓↓↓↓↓↓↓↓ 第%v个仓库，名字为%v，为私有库，账户、Token已配置，开始同步", i+1, repo.RepoName)
-						errSr := SyncRepo(repo.RepoURL, fmt.Sprintf("%v/%v", repoBaseDir, repoDirName), repo.GitAccount, repo.GitToken)
+						errSr := SyncRepo(repo.RepoURL, fmt.Sprintf("%v/%v", repoBaseDir, repo.RepoName), repo.GitAccount, repo.GitToken)
 						if errSr == nil {
 							succCnt += 1
 						} else {
