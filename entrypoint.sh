a71f5a1e0ec31da23e2abab7febd35145ef25599
Akira <e.akimoto.akira@gmail.com>
2021-11-09 15:16:22 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,46 +23,58 @@ function golangInit() {
 
 if [ "$1" ]; then
   up_cmd=$1
-  if [ "${APK_REPO}" ]; then
-    sed -i "s/dl-cdn.alpinelinux.org/${APK_REPO}/g" /etc/apk/repositories
+  if [ "$APK_REPO" ]; then
+    sed -i "s/dl-cdn.alpinelinux.org/$APK_REPO/g" /etc/apk/repositories
   fi
   if [ "$APK_ADD_PKG"]; then
     apk add "$(echo $APK_ADD_PKG | tr "&" " ")"
   fi
   if [ "$INIT_ENVS" ]; then
     for env in $(echo "$INIT_ENVS" | tr "&" " "); do
-      "${env}Init"
+      "$envInit"
       if [ $? -ne 0 ]; then
-        echo "${env}环境初始化出错❌，重启后继续尝试初始化"
+        echo "[$env]环境初始化出错❌，重启后继续尝试初始化"
         exit 1
       else
-        echo "${env}环境初始化完成✅"
+        echo "[$env]环境初始化完成✅"
       fi
     done
   fi
 fi
 # 是否指定了仓库信息配置文件的路径，未制定，自动寻找MNT_DIR根木repos.json
-if [ -z "$REPOS_CONFIG" ];then
+if [ -z "$REPOS_CONFIG" ]; then
   export REPOS_CONFIG=$MNT_DIR/repos.json
 fi
-echo "-----------------------------------------读取$REPOS_CONFIG仓库相关配置，并进行执行配置-----------------------------------------"
+echo "-----------------------------------------读取[$REPOS_CONFIG]仓库相关配置，并进行执行配置-----------------------------------------"
 # 同步仓库
 repo_sync
 
-echo "执行仓库入口脚本"
+echo ""
+echo ">>>>>>>>>>>>执行仓库入口脚本"
 for repoInx in $(cat $REPOS_CONFIG | jq .repos | jq 'keys|join(" ")' | sed "s/\"//g"); do
   cd "$REPOS_DIR"
-  repoName=$(cat $REPOS_CONFIG | jq ".repos | .[$repoInx] | .repo_name")
-  repoBranch=$(cat $REPOS_CONFIG | jq ".repos | .[$repoInx] | .repo_branch")
+  repoName=$(cat $REPOS_CONFIG | jq -r ".repos | .[$repoInx] | .repo_name")
+  repoBranch=$(cat $REPOS_CONFIG | jq -r ".repos | .[$repoInx] | .repo_branch")
   cd "$REPOS_DIR/$repoName"
-  echo "切换到 $repoBranch 分支..."
+  if [ -z "$repoBranch" ]; then
+    echo "未指定[$repoName]仓库分支，使用当前默认分支"
+  else
+    echo "[$repoName]仓库切换到指定的[$repoBranch]分支..."
+  fi
   git checkout $repoBranch
-  echo "执行$repoName仓库下面的程序入口脚本"
-  sh iou-entry.sh
+
+  if [ -f "$REPOS_DIR/$repoName/iou-entry.sh" ]; then
+    echo "执行[$repoName]仓库下面的程序入口脚本"
+    sh iou-entry.sh
+  else
+    echo "仓库[$repoName]不存在iou-entry.sh入口脚本文件，跳过..."
+  fi
 done
-echo "--------------------------------------------------$REPOS_CONFIG配置结束---------------------------------------------------"
+
+echo "--------------------------------------------------[$REPOS_CONFIG]配置结束---------------------------------------------------"
 
 firstFile="y"
+echo "05 * * * * entrypoint.sh >> $MNT_DIR/entrypoint.log 2>&1 " >entrypoint_cron.sh
 for cronFile in $(ls "$CRON_FILE_PATH" | grep ".sh" | tr "\n" " "); do
   if [ $firstFile == "y" ]; then
     echo "#$cronFile cron list" >"$CRON_FILE_PATH/merge_all_cron.sh"
--- repo_sync/repo_sync.go
+++ repo_sync/repo_sync.go
@@ -7,7 +7,6 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	rs "repo_sync/utils"
@@ -34,7 +33,7 @@ func main() {
 
 func readRepoConfig(repoConfigJson string, repoBaseDir string) {
 	if rs.Exists(repoConfigJson) {
-		log.Printf("检测到仓库配置文件 %v，开始同步仓库操作。", repoConfigJson)
+		fmt.Printf("检测到仓库配置文件 %v，开始同步仓库操作。", repoConfigJson)
 		var repoConfig rs.ReposConfig
 		f, _ := ioutil.ReadFile(repoConfigJson)
 		if err := json.Unmarshal(f, &repoConfig); err == nil {
@@ -42,7 +41,7 @@ func readRepoConfig(repoConfigJson string, repoBaseDir string) {
 			for i, repo := range repoConfig.Repos {
 				if repo.RepoPrivate {
 					if repo.GitAccount != "" && repo.GitToken != "" {
-						log.Printf("↓↓↓↓↓↓↓↓↓↓↓↓ 第%v个仓库，名字为%v，为私有库，账户、Token已配置，开始同步", i+1, repo.RepoName)
+						fmt.Printf("↓↓↓↓↓↓↓↓↓↓↓↓ 第%v个仓库，名字为%v，为私有库，账户、Token已配置，开始同步", i+1, repo.RepoName)
 						errSr := SyncRepo(repo.RepoURL, fmt.Sprintf("%v/%v", repoBaseDir, repo.RepoName), repo.GitAccount, repo.GitToken)
 						if errSr == nil {
 							succCnt += 1
@@ -50,11 +49,11 @@ func readRepoConfig(repoConfigJson string, repoBaseDir string) {
 							failCnt += 1
 						}
 					} else {
-						log.Printf("第%v个仓库，名字为%v，为私有库，但是账户、Token未配置，同步失败", i+1, repo.RepoName)
+						fmt.Printf("第%v个仓库，名字为%v，为私有库，但是账户、Token未配置，同步失败", i+1, repo.RepoName)
 						failCnt += 1
 					}
 				} else {
-					log.Printf("↓↓↓↓↓↓↓↓↓↓↓↓ 第%v个仓库，名字为%v，为公开仓库，开始同步", i+1, repo.RepoName)
+					fmt.Printf("↓↓↓↓↓↓↓↓↓↓↓↓ 第%v个仓库，名字为%v，为公开仓库，开始同步", i+1, repo.RepoName)
 					errSr := SyncRepo(repo.RepoURL, repo.RepoName, repo.GitAccount, repo.GitToken)
 					if errSr == nil {
 						succCnt += 1
@@ -64,27 +63,27 @@ func readRepoConfig(repoConfigJson string, repoBaseDir string) {
 				}
 
 			}
-			log.Printf("\n仓库同步已完成！成功%v个,失败%v个", succCnt, failCnt)
+			fmt.Printf("\n仓库同步已完成！成功%v个,失败%v个", succCnt, failCnt)
 		} else {
-			log.Printf("读取仓库配置文件出错，跳过同步仓库操作。请检查 %v 文件配置是否正确", repoConfigJson)
+			fmt.Printf("读取仓库配置文件出错，跳过同步仓库操作。请检查 %v 文件配置是否正确", repoConfigJson)
 		}
 	} else {
-		log.Printf("仓库配置文件 %v 不存在，跳过同步仓库操作。", repoConfigJson)
+		fmt.Printf("仓库配置文件 %v 不存在，跳过同步仓库操作。", repoConfigJson)
 	}
 }
 func SyncRepo(repoUrl string, repoPath string, gitAccount string, gitToken string, ) error {
 	if rs.Exists(repoPath) {
-		log.Printf("脚本仓库目录已存在，执行pull")
+		fmt.Printf("脚本仓库目录已存在，执行pull")
 		return pullRepo(repoPath, gitAccount, gitToken)
 	} else {
-		log.Printf("脚本仓库目录不存在，执行clone")
+		fmt.Printf("脚本仓库目录不存在，执行clone")
 		return cloneRepo(repoUrl, repoPath, gitAccount, gitToken)
 	}
 
 }
 
 func cloneRepo(url string, directory string, gitAccount string, gitToken string) error {
-	log.Printf("git clone %s to %s", url, directory)
+	fmt.Printf("git clone %s to %s", url, directory)
 
 	r, err := git.PlainClone(directory, false, &git.CloneOptions{
 		Auth: &http.BasicAuth{
@@ -136,11 +135,11 @@ func pullRepo(path string, gitAccount string, gitToken string) error {
 		}})
 	if errPull != nil {
 		if errPull.Error() == "already up-to-date" {
-			log.Printf("已经是最新代码，暂无更新。")
+			fmt.Printf("已经是最新代码，暂无更新。")
 		} else if errPull.Error() == "authentication required" {
-			log.Printf("用户密码登陆失败，更新失败。")
+			fmt.Printf("用户密码登陆失败，更新失败。")
 		} else {
-			log.Printf(errPull.Error())
+			fmt.Printf(errPull.Error())
 			return errPull
 		}
 	} else {
@@ -151,7 +150,7 @@ func pullRepo(path string, gitAccount string, gitToken string) error {
 
 		commit, errC := r.CommitObject(ref.Hash())
 		rs.CheckIfError(errC)
-		log.Printf("%v", commit)
+		fmt.Printf("%v", commit)
 	}
 	return nil
 }
@@ -170,13 +169,13 @@ func resetHard(path string) {
 	command.Stdout = &outInfo
 	err := command.Start()
 	if err != nil {
-		log.Printf(err.Error())
+		fmt.Printf(err.Error())
 	}
 	if err = command.Wait(); err != nil {
-		log.Printf(err.Error())
+		fmt.Printf(err.Error())
 	} else {
 		//fmt.Println(command.ProcessState.Pid())
 		//fmt.Println(command.ProcessState.Sys().(syscall.WaitStatus).ExitStatus())
-		log.Printf("还原本地修改（新增文件不受影响）防止更新冲突...")
+		fmt.Printf("还原本地修改（新增文件不受影响）防止更新冲突...")
 	}
 }
