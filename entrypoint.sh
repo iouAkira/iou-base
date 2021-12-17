e9199a2b697ae14c2a2c3c4e7dc5b3b457b52299
Akira <e.akimoto.akira@gmail.com>
2021-12-17 13:34:51 +0800

    修复clone仓库失败导致目录不存在，仍然执行iou-entry.sh入口文件的问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,41 +61,45 @@ for repoInx in $(cat $REPOS_CONFIG | jq .repos | jq 'keys|join(" ")' | sed "s/\"
     repoName=$(cat $REPOS_CONFIG | jq -r ".repos | .[$repoInx] | .repo_name")
     repoBranch=$(cat $REPOS_CONFIG | jq -r ".repos | .[$repoInx] | .repo_branch")
     repoEntrypoint=$(cat $REPOS_CONFIG | jq -r ".repos | .[$repoInx] | .repo_entrypoint")
-    cd "$REPOS_DIR/$repoName"
-    if [ -z $(echo $repoBranch | sed "s/null//g") ]; then
-        echo "[$repoName] 仓库未指定分支，使用当前默认分支"
-    else
-        echo "[$repoName] 仓库切换到指定的[$repoBranch]分支..."
-        git checkout $repoBranch | sed -e "s/^/[$repoName] /"
-    fi
-    if [ $(echo $repoEntrypoint | sed "s/null//g") ]; then
-        echo "[$repoName] 仓库已配置指定的入口shell脚本文件"
-        if expr "$repoEntrypoint" : 'http.*' &>/dev/null; then
-            echo "[$repoName] 仓库配置指定的入口shell脚本为远程脚本，开始下载远程脚本 $repoEntrypoint"
-            wget -O iou-entry.sh "$repoEntrypoint" | sed -e "s/^/[$repoName]/"
-            echo "[$repoName] 仓库配置指定的入口shell脚本下载完成，开始执行..."
-            sh iou-entry.sh | sed -e "s/^/[$repoName\/iou-entry.sh] /"
-            echo "[$repoName] 仓库配置指定的入口shell脚本，执行结束..."
+    if [ -d "$REPOS_DIR/$repoName" ]; then
+        cd "$REPOS_DIR/$repoName"
+        if [ -z $(echo $repoBranch | sed "s/null//g") ]; then
+            echo "[$repoName] 仓库未指定分支，使用当前默认分支"
         else
-            if [ ! -f "$repoEntrypoint" ]; then
-                echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，但是挂载文件$repoEntrypoint不存在，跳过..."
-            else
-                echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，开始执行..."
-                cp -rf "$repoEntrypoint" ./iou-entry.sh
+            echo "[$repoName] 仓库切换到指定的[$repoBranch]分支..."
+            git checkout $repoBranch | sed -e "s/^/[$repoName] /"
+        fi
+        if [ $(echo $repoEntrypoint | sed "s/null//g") ]; then
+            echo "[$repoName] 仓库已配置指定的入口shell脚本文件"
+            if expr "$repoEntrypoint" : 'http.*' &>/dev/null; then
+                echo "[$repoName] 仓库配置指定的入口shell脚本为远程脚本，开始下载远程脚本 $repoEntrypoint"
+                wget -O iou-entry.sh "$repoEntrypoint" | sed -e "s/^/[$repoName]/"
+                echo "[$repoName] 仓库配置指定的入口shell脚本下载完成，开始执行..."
                 sh iou-entry.sh | sed -e "s/^/[$repoName\/iou-entry.sh] /"
-                echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，开始结束..."
+                echo "[$repoName] 仓库配置指定的入口shell脚本，执行结束..."
+            else
+                if [ ! -f "$repoEntrypoint" ]; then
+                    echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，但是挂载文件$repoEntrypoint不存在，跳过..."
+                else
+                    echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，开始执行..."
+                    cp -rf "$repoEntrypoint" ./iou-entry.sh
+                    sh iou-entry.sh | sed -e "s/^/[$repoName\/iou-entry.sh] /"
+                    echo "[$repoName] 仓库配置指定的入口shell脚本为挂载脚本文件，开始结束..."
+                fi
+            fi
+        else
+            if [ -f "$REPOS_DIR/$repoName/iou-entry.sh" ]; then
+                echo "[$repoName] 仓库为默认入口shell脚本，开始执行..."
+                sh iou-entry.sh | sed -e "s/^/[$repoName\/iou-entry.sh] /"
+                echo "[$repoName] 仓库为默认入口shell脚本，执行结束..."
+            else
+                echo "[$repoName] 仓库为默认入口shell脚本iou-entry.sh不存在，跳过..."
             fi
         fi
+        echo "-e"
     else
-        if [ -f "$REPOS_DIR/$repoName/iou-entry.sh" ]; then
-            echo "[$repoName] 仓库为默认入口shell脚本，开始执行..."
-            sh iou-entry.sh | sed -e "s/^/[$repoName\/iou-entry.sh] /"
-            echo "[$repoName] 仓库为默认入口shell脚本，执行结束..."
-        else
-            echo "[$repoName] 仓库为默认入口shell脚本iou-entry.sh不存在，跳过..."
-        fi
+        echo "[$repoName] 仓库目录不存在，可能clone失败，跳过..."
     fi
-    echo "-e"
 done
 
 echo "========================================================$REPOS_CONFIG]配置结束================================================="
